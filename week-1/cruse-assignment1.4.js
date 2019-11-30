4c115b8c87ac4c12ee62fc6702bad673eaa17237
Jess Cruse <git@crusemob.com>
2019-11-29 18:06:47 -0600

    adding attrbution to hello-world and assignment 1.4

---
+++ week-1/cruse-assignment1.4.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Jess Cruse
+; Date: 29 November 2019
+; Modified By: <Your name>
+; Description: The purpose of this exercise 
+; is to demonstration the use of primitive types
+; in a JavaScript program in VS Code. 
+;===========================================
+*/ 
+
+//Variables required for exercise
+var firstName = "Jess";
+var lastName = "Cruse";
+var age = 39;
+var street = "Wall Street";
+var city = "New York";
+var zipCode = "10005";
+
+//output
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+console.log (street);
+console.log (city);
+console.log (zipCode);
