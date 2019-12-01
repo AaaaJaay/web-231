fc6840c52f336f44e2de6c9614474f9ae0450f5c
Jess Cruse <git@crusemob.com>
2019-12-01 16:00:14 -0600

    response to saeou-assignment 1.1

---
+++ week-1/saeou-ssignment1.1.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Joann Saeou
+; Date: 26 november 2019
+; Modified By: Jess Cruse
+; Description: Javascript Variables and Values while finding the 2 errors
+============================================
+*/
+
+var mystring1 = "I am learning";
+var mystring2= "Javascript with Bellevue University";
+console.log(mystring1 + " " + mystring2); //changed document.write to console.log and change the 2nd mystring1 to mystring2 and added space
+
+var myNum1=7;
+var myNum2=10; //the N in mynum2 was not capitol and it's case sensitive
+if(myNum1<myNum2){
+    console.log("I am starting to like learning about Javascript");
+}
