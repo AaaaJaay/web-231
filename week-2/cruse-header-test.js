9c6082909f80c81e0076d37b638da08f7c3b165a
Jess Cruse <git@crusemob.com>
2019-12-03 19:10:49 -0600

    adding required root directory files and Exercise 2-2 - Headers

---
+++ week-2/cruse-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   25 June 2017
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./cruse-header.js');
+
+console.log(header.display("Jess", "Cruse", "Exercise 2.2 - Header"));
