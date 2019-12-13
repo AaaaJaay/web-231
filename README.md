273fe94df982b0f7d86e52e3dbc4829efac54c4d
AaaaJaay <git@crusemob.com>
2019-12-12 20:27:30 -0600

    discussion-3.1
    For this week’s discussion board assignment, create a simple JavaScript program using either a for, while, if, or switch statement with at least two errors.  The program should not execute and there should be at least two errors in the code you post to Blackboard.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Introduction to JavaScript
 #contributors
 Professor Krasso - Bellevue University
 Jess Cruse
+
+12 Dec 2019 - cruse-discussion-3.1.js added as part of week 3
---
+++ week-3/cruse-discussion-3.1.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Discussion 3.1
+; Author: Jess Cruse
+; Date: 12 Dec 2019
+; Modified By: Jess Cruse
+; Description: This program demonstrates the
+; use of else if in JavaScript.  It has 2+
+; errors which need resolved.
+;===========================================
+*/
+
+var x = 2;
+var y = 4;
+
+//evaluating the relationship of the numbers
+if (x == y) {
+  console.log("x and y are the same");  //response of the numbers are the same
+}
+else if (x > y); {
+  console.log(x + " is greater than " + y); //response if x is greater then y
+}
+else if (y > x) (
+  console.log(y + " is greater than " + x);  //response if y is greater then x
+)
+else
+  console.log("So sorry! Something didn't go as expected.");  //error message if above logic isn't met
