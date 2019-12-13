6c45077d539ef7fe7e8256ee89cde8df9c3806ac
AaaaJaay <git@crusemob.com>
2019-12-12 21:03:56 -0600

    Discussion Board 3.1 - Control Statements
    Added header and fixed a typo

--- week-3/cruse-discussion-3.1.js
+++ week-3/cruse-discussion-3.1.js
@@ -10,6 +10,11 @@
 ;===========================================
 */
 
+//Header
+const header = require('./cruse-header.js');
+console.log(header.display("Jess", "Cruse", "Discussion 3.1"));
+
+//Start Program
 var x = 2;
 var y = 4;
 
@@ -18,10 +23,12 @@ if (x == y) {
   console.log("x and y are the same");  //response of the numbers are the same
 }
 else if (x > y); {
-  console.log(x + " is greater than " + y); //response if x is greater then y
+  console.log(x + " is greater than " + y); //response if x is greater than y
 }
 else if (y > x) (
-  console.log(y + " is greater than " + x);  //response if y is greater then x
+  console.log(y + " is greater than " + x);  //response if y is greater than x
 )
 else
   console.log("So sorry! Something didn't go as expected.");  //error message if above logic isn't met
+
+  //END Program
