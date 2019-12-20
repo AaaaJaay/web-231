37e4368a126964f1ca29fc58e73d6d809bdd49b4
AaaaJaay <git@crusemob.com>
2019-12-19 20:50:19 -0600

    Discussion Board 4.1 - Arrays

---
+++ week-4/cruse-discussion-4.1.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Discussion Board 4.1 - Arrays
+; Author: Jess Cruse
+; Date: 19 Dec 2019
+; Modified By: Jess Cruse
+; Description: This program compares two number and provides error handling for if the numbers match or not
+;===========================================
+*/
+
+//Header
+const header = require('../header.js');
+console.log(header.display("Jess", "Cruse", "Discussion Board 4.1 - Arrays"));
+
+//START PROGRAM
+
+var myNumbers = (1; 2; 3; 4; 5; 6) console.log("My Numbers are " + myNumbers);
+
+//END PROGRAM
